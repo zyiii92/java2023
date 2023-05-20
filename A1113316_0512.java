5807e25d07205224f4facc6b596fd4820944db68
zyiii92 <125672164+zyiii92@users.noreply.github.com>
2023-05-20 14:28:54 +0800

    Add files via upload

--- A1113316_0512.java
+++ A1113316_0512.java
@@ -65,58 +65,10 @@ public class A1113316_0512 {
         System.out.println("請輸入同時光顧的顧客數目:");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        
+
         for(int i=0;i<num;i++){
             dumpThread customer = new dumpThread();
             customer.start();
         }
     }
 }
-
-//     public synchronized void run() {
-//         int count = (int) (Math.random() * 41) + 10;
-//         if (count % 3 == 0) {
-//             if (pork - count >= 0) {
-//                 pork -= count;
-//             } else {
-//                 System.out.println("豬肉水餃已售完!");
-//             }
-//         }
-//         if (count % 3 == 1) {
-//             if (beef - count >= 0) {
-//                 beef -= count;
-//             } else {
-//                 System.out.println("牛肉水餃已售完!");
-//             }
-//         }
-//         if (count % 3 == 2) {
-//             if (vegetable - count >= 0) {
-//                 vegetable -= count;
-//             } else {
-//                 System.out.println("蔬菜水餃已售完!");
-//             }
-//         }
-
-//         try{
-//         Thread.sleep(3000);
-//         }catch(InterruptedException e){
-
-//         }
-//     }
-
-// }
-
-// public class A1113316_0512 {
-//     public static void main(String[] args) {
-
-//         Scanner sc = new Scanner(System.in);
-//         System.out.print("輸入顧客數目:");
-//         int customer = sc.nextInt();
-
-//         for (int i = 1; i <= customer; i++) {
-//             dumpThread a = new dumpThread(); 
-//             a.start();
-
-//         }
-//     }
-// }
